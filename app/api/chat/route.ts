6f13651bc823f77393077b915312e39696557982
Sawyer Hood <sawyerjhood@gmail.com>
2024-04-09 19:23:06 -0700

    Make generation an edge function

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -17,6 +17,10 @@ const anthropic = new Anthropic({
 const HAIKU_MODEL = "claude-3-haiku-20240307";
 const SONNET_MODEL = "claude-3-sonnet-20240229";
 
+export const config = {
+  runtime: "edge",
+};
+
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
   const { messages } = await req.json();
