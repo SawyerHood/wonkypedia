80b498748136277e3207876d66ba1cdd27ea0be7
Sawyer Hood <sawyerjhood@gmail.com>
2024-04-09 19:23:54 -0700

    Really make it edge

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -17,9 +17,7 @@ const anthropic = new Anthropic({
 const HAIKU_MODEL = "claude-3-haiku-20240307";
 const SONNET_MODEL = "claude-3-sonnet-20240229";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
