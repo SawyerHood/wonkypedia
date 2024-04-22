8df3fbaf4f6fdf6fc8444a7ec0d2452a21ae6c3d
Sawyer Hood <sawyerjhood@gmail.com>
2024-04-22 09:04:55 -0700

    Try pipeline connect

--- db/client.ts
+++ db/client.ts
@@ -1,7 +1,8 @@
 import * as schema from "@/drizzle/schema";
-import { Pool } from "@neondatabase/serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
+neonConfig.pipelineConnect = false;
 const pool = new Pool({ connectionString: process.env.POSTGRES_URL! });
 export const db = drizzle(pool, { schema });
 
