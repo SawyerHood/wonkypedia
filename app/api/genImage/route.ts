ba139a2298e37529bfa58a75df09a2cbebf8a8ed
Sawyer Hood <sawyerjhood@gmail.com>
2024-04-22 02:03:41 -0700

    Force edge

--- app/api/genImage/route.ts
+++ app/api/genImage/route.ts
@@ -1,9 +1,11 @@
 import { genAndUploadImage } from "@/generation/image";
 
-export const runtime = "nodejs";
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { prompt } = await req.json();
+  console.log("start");
   const url = await genAndUploadImage(prompt);
+  console.log("end");
   return new Response(JSON.stringify({ url }));
 }
