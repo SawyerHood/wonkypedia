cf58c6e8422bcbca54dc0187161f42b18a65d879
Sawyer Hood <sawyerjhood@gmail.com>
2024-04-23 14:17:48 -0700

    Auto update homepage

--- app/api/genHomePage/route.ts
+++ app/api/genHomePage/route.ts
@@ -2,7 +2,16 @@ import { generateHomepage } from "@/generation/homepage";
 import { kv } from "@vercel/kv";
 import { revalidatePath } from "next/cache";
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
+  const authHeader = req.headers.get("authorization");
+  console.log(authHeader);
+  console.log(process.env.CRON_SECRET);
+  if (
+    !process.env.CRON_SECRET ||
+    authHeader !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return new Response(null, { status: 401 });
+  }
   const homepage = await generateHomepage();
   kv.set("homepage", homepage);
   revalidatePath("/api/homepage");
--- scripts/updateHomepage.ts
+++ scripts/updateHomepage.ts
@@ -1,6 +1,9 @@
 async function updateHomepage() {
   const res = await fetch("http://localhost:3000/api/genHomePage", {
-    method: "POST",
+    method: "GET",
+    headers: {
+      authorization: `Bearer ${process.env.CRON_SECRET}`,
+    },
   });
   console.log(await res.json());
   process.exit(0);
